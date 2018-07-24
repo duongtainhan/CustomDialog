ba1222ffd71df29ca5d8b8ac6a0319711148f342
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-07-25 02:32:55 +0700

    click button Login, Cancel

--- app/src/main/java/com/example/duongtainhan555/customdialog/MainActivity.java
+++ app/src/main/java/com/example/duongtainhan555/customdialog/MainActivity.java
@@ -4,7 +4,10 @@ import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,8 +27,26 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     private void DialogLogin(){
-        Dialog dialog = new Dialog(this);
+        final Dialog dialog = new Dialog(this);
         dialog.setContentView(R.layout.custom_dialog);
+        //Anh xa
+        final EditText edUser = dialog.findViewById(R.id.edUser);
+        final EditText edPass = dialog.findViewById(R.id.edPass);
+        Button btnLogin = dialog.findViewById(R.id.btnLogin);
+        Button btnCancel = dialog.findViewById(R.id.btnCancel);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String result = (edUser.getText().toString().equals("Nhan")&&edPass.getText().toString().equals("1997"))?"Login success":"Login failed";
+                Toast.makeText(MainActivity.this,result,Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              dialog.cancel();
+            }
+        });
         dialog.show();
     }
 }
