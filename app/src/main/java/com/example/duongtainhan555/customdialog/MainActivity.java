4f0dfb517dc89f15a7ed55748766fe88062b823f
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-07-25 02:21:14 +0700

    Show dialog

--- app/src/main/java/com/example/duongtainhan555/customdialog/MainActivity.java
+++ app/src/main/java/com/example/duongtainhan555/customdialog/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.duongtainhan555.customdialog;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txtTitle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Anh xa
+        txtTitle = findViewById(R.id.txtTitle);
+
+        txtTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogLogin();
+            }
+        });
+    }
+    private void DialogLogin(){
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.custom_dialog);
+        dialog.show();
     }
 }
